53604b4adeda56ea173a32d575a28d4757298ac6
noman1303 <belimnoman1303@gmail.com>
2026-02-25 16:00:08 +0530

    Updated

--- AlphabateScrollingUIKit/FindConnectionTableViewCell.swift
+++ AlphabateScrollingUIKit/FindConnectionTableViewCell.swift
@@ -14,12 +14,21 @@ class FindConnectionTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var SendUIVIew: UIView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var reportButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        nameLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        addressLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        addressLabel.textColor = .secondaryLabel
         MainUIVIew.layer.cornerRadius = 15
-        ReportUIVIew.layer.cornerRadius = 15
-        SendUIVIew.layer.cornerRadius = 15
+        sendButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        sendButton.setTitleColor(.systemBlue, for: .normal)
+        reportButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        reportButton.setTitleColor(.label, for: .normal)
+        ReportUIVIew.layer.cornerRadius = 10
+        SendUIVIew.layer.cornerRadius = 10
         
     }
 
--- AlphabateScrollingUIKit/MemberTableViewCell.swift
+++ AlphabateScrollingUIKit/MemberTableViewCell.swift
@@ -19,7 +19,7 @@ class MemberTableViewCell: UITableViewCell {
     @IBOutlet weak var BGView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+//        // Initialization code
         CallUIVIew.layer.cornerRadius = 15
         smsUIVIew.layer.cornerRadius = 15
         WhatsAppui.layer.cornerRadius = 15
